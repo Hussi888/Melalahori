21366054f18da3bcd07803c89f3dd369c31d48d6
Altmash <altmashrana.303@gmail.com>
2020-01-05 19:43:53 +0500

    Login and Signup FireBAse Authentication and FireStore added.

--- app/src/main/java/com/example/melalahori/SplashScreen.java
+++ app/src/main/java/com/example/melalahori/SplashScreen.java
@@ -2,13 +2,26 @@ package com.example.melalahori;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private  static int splashtime=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent signup_intent = new Intent(SplashScreen.this, Signup.class);
+                startActivity(signup_intent);
+                finish();
+            }
+        },splashtime);
     }
 }
